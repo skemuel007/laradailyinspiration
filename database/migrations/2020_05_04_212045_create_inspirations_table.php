b1ac26531b0eb28e21cfde2ba37b7c979c686548
skemuel007 <skemuel007@gmail.com>
2020-05-04 22:21:46 +0100

    Initial Commit

---
+++ database/migrations/2020_05_04_212045_create_inspirations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInspirationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inspirations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inspirations');
+    }
+}
